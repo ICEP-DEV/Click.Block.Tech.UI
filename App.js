134f9a62ddd10189c27c2f26633209fe89d5f188
Awelani-AwelaniTshilonga <awetshilonga@gmail.com>
2024-10-04 09:04:21 +0200

    backend connected

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationBar from './screens/navigationBar'; 
-import { createStackNavigator } from '@react-navigation/stack';
 
 // Create a Stack Navigator
 const Stack = createStackNavigator();
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from './Screens/Login';
+import Login from './screens/Login';
 
 //rimport HomeScreen from './src/screens/HomeScreen'; // A new screen for navigation
 
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
